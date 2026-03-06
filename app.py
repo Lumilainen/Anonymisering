9b6a3f51642bcc3406fccfb79bcc9bc28daa903a
Lumilainen <mln@kubal.se>
2026-03-06 14:01:57 +0000

    streamlit deployment fix

--- app.py
+++ app.py
@@ -2,20 +2,11 @@ import streamlit as st
 from docx import Document
 from io import BytesIO
 import spacy
-import subprocess
-import sys
-
-# säker laddning av spaCy-modell
-MODEL_NAME = "sv_core_news_sm"
-
-try:
-    nlp = spacy.load(MODEL_NAME)
-except:
-    subprocess.check_call([sys.executable, "-m", "spacy", "download", MODEL_NAME])
-    nlp = spacy.load(MODEL_NAME)
 
 from main import anonymize_docx, scan_document_for_persons
 
+# ladda spaCy-modell
+nlp = spacy.load("sv_core_news_sm")
 
 st.set_page_config(
     page_title="Word anonymiserare",
@@ -29,7 +20,7 @@ st.write(
 """
 Ladda upp ett Word-dokument (.docx) för att anonymisera personuppgifter.
 
-**Integritet**
+Integritet:
 - Dokument lagras inte
 - Bearbetning sker endast i minnet
 - Filer raderas automatiskt efter nedladdning
@@ -43,19 +34,21 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file:
 
+    # läs fil i minnet
     file_bytes = uploaded_file.read()
 
     doc_stream = BytesIO(file_bytes)
 
     doc = Document(doc_stream)
 
+    # identifiera personer
     persons = sorted(scan_document_for_persons(doc))
+
+    selected_persons = []
 
     if persons:
 
         st.subheader("Identifierade personer")
-
-        selected_persons = []
 
         for person in persons:
 
@@ -65,8 +58,6 @@ if uploaded_file:
                 selected_persons.append(person)
 
     else:
-
-        selected_persons = []
 
         st.info("Inga personer identifierades")
 
@@ -86,6 +77,7 @@ if uploaded_file:
         with st.spinner("Anonymiserar dokument..."):
 
             input_stream = BytesIO(file_bytes)
+
             output_stream = BytesIO()
 
             anonymize_docx(
